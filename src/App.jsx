1a4c316322d67338dcc463f6d97bbc50df3f5f68
biancapcr <biancacristina.pocora@gmail.com>
2025-08-30 21:50:54 +0200

    [feat] add articles list

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,16 @@
+// mount app
 import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
 
+// creazione dei dati
+const BlogArticles = [
+  { id: 1, 
+    title: "Props vs State: Clear Differences" },
+  { id: 2, 
+    title: "Handling Events in React" },
+  { id: 3, 
+    title: "JSX Basics: From HTML to Components" },
+];
 
 export default App
